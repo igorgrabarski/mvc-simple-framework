0b9ab62eff21e188dea8cf2afc030c292511f174
Igor Grabarski <igorgrabarski@gmail.com>
2017-12-06 23:11:43 -0500

    created Router class

--- Core/Router.php
+++ Core/Router.php
@@ -15,19 +15,70 @@ class Router {
 	protected $routes = [];
 
 
+	/** Parameters from the matched route
+	 * @var array
+	 */
+	protected $params = [];
+
+
 	/** Add a route to the Routing Table
+	 *
 	 * @param $route
 	 * @param $params
 	 */
-	public function add($route, $params){
-		$this->routes[$route] = $params;
+	public function add( $route, $params = [] ) {
+		// Convert route to a regex: escape forward slashes
+		$route = preg_replace( '/\//', '\\/', $route );
+
+		// Convert variables
+		$route = preg_replace( '/\{([a-z]+)\}/', '(?P<\1>[a-z]+)', $route );
+
+		// Convert variables with custom regular expressions
+		$route = preg_replace('/\{([a-z]+):([^\}]+)\}/', '(?P<\1>\2)', $route);
+
+		// Add start and end delimiters, and case insensitive flag
+		$route = '/^' . $route . '$/i';
+
+		$this->routes[ $route ] = $params;
 	}
 
 
 	/** Get all the routes from the Routing Table
 	 * @return array
 	 */
-	public function getRoutes(){
+	public function getRoutes() {
 		return $this->routes;
+	}
+
+
+	/** Match the route to the route in the routing table.
+	 *  Set the instance $params property, if a route is found.
+	 *
+	 * @param $url
+	 *
+	 * @return bool
+	 */
+	public function match( $url ) {
+
+		foreach ( $this->routes as $route => $params ) {
+			if ( preg_match( $route, $url, $matches ) ) {
+				foreach ( $matches as $key => $match ) {
+					if ( is_string( $key ) ) {
+						$params[ $key ] = $match;
+					}
+				}
+
+				$this->params = $params;
+
+				return true;
+			}
+		}
+
+		return false;
+	}
+
+
+	public function getParams() {
+		return $this->params;
 	}
 }
--- public/index.php
+++ public/index.php
@@ -7,4 +7,20 @@ $router = new Router();
 // Add the routes
 $router->add('', [ 'controller' => 'Home', 'action' => 'index' ]);
 $router->add('posts', [ 'controller' => 'Posts', 'action' => 'index' ]);
-$router->add('posts/new', [ 'controller' => 'Posts', 'action' => 'new' ]);
+//$router->add('posts/new', [ 'controller' => 'Posts', 'action' => 'new' ]);
+$router->add('{controller}/{action}');
+$router->add('admin/{action}/{controller}');
+$router->add('{action}/{controller}/{id:\d+}');
+
+
+
+$url = $_SERVER['QUERY_STRING'];
+
+if($router->match($url)){
+	echo '<pre>';
+	print_r($router->getParams());
+	echo '</pre>';
+}
+else {
+	echo 'Error 404! Page not found!';
+}
